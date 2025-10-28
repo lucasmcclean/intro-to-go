e07e810829ab6a37ee8b765bcc896fd8b33dc296
Lucas McClean <121735106+lucasmcclean@users.noreply.github.com>
2025-10-28 10:13:40 -0400

    restore: main function

--- main.go
+++ main.go
@@ -1,13 +1,43 @@
 package main
 
-// Create context
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
 
-// Create server
+	"github.com/lucasmcclean/intro-to-go/server"
+)
 
-// Go run server
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
 
-// Create waitgroup
+	srv := server.New()
 
-// Go run shutdown
+	go func() {
+		fmt.Printf("Starting server on %s\n", srv.Addr)
+		err := srv.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			fmt.Printf("Server error: %v", err)
+		}
+	}()
 
-// Wait on waitgroup
+	var wg sync.WaitGroup
+
+	wg.Go(func() {
+		<-ctx.Done()
+		shutdownCtx := context.Background()
+		shutdownCtx, cancel := context.WithTimeout(shutdownCtx, 10*time.Second)
+		defer cancel()
+		err := srv.Shutdown(shutdownCtx)
+		if err != nil {
+			fmt.Printf("Error shutting down server: %s\n", err)
+		}
+	})
+
+	wg.Wait()
+}
